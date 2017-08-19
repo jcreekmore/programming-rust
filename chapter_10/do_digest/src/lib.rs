3a2d943944ee441293b05efba0b81470cfd19b7c
Jonathan Creekmore <jonathan@thecreekmores.org>
2017-08-19 08:14:09 -0700

    chapter 10, do_digest crate

---
+++ chapter_10/do_digest/src/lib.rs
@@ -0,0 +1,44 @@
+extern crate digest;
+use digest::Digest;
+
+pub trait DoDigest<D: Digest> {
+    fn update_digest(&self, &mut D);
+}
+
+impl<D> DoDigest<D> for String
+    where D: Digest
+{
+    fn update_digest(&self, digest: &mut D) {
+        digest.input(self.as_bytes());
+    }
+}
+
+impl<D> DoDigest<D> for str
+    where D: Digest
+{
+    fn update_digest(&self, digest: &mut D) {
+        digest.input(self.as_bytes());
+    }
+}
+
+impl<D, X> DoDigest<D> for [X]
+    where D: Digest,
+          X: DoDigest<D>,
+{
+    fn update_digest(&self, digest: &mut D) {
+        for x in self {
+            x.update_digest(digest);
+        }
+    }
+}
+
+impl<D, X> DoDigest<D> for Vec<X>
+    where D: Digest,
+          X: DoDigest<D>,
+{
+    fn update_digest(&self, digest: &mut D) {
+        for x in self {
+            x.update_digest(digest);
+        }
+    }
+}
